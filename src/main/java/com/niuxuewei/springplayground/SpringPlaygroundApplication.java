95599d43530afc95a3b182745242c334ccb19fcc
xavierniu <a@nxw.name>
2019-02-15 20:57:58 +0800

    init

---
+++ src/main/java/com/niuxuewei/springplayground/SpringPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.niuxuewei.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlaygroundApplication.class, args);
+    }
+
+}
+
