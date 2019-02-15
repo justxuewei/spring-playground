29eb7fdbdb05f91694deaa18dcf5f32817b500a7
xavierniu <a@nxw.name>
2019-02-15 21:52:32 +0800

    chapter 16: SpringBoot注解把配置文件自动映射到属性和实体类实战

--- src/main/java/com/niuxuewei/springplayground/controller/SimpleController.java
+++ src/main/java/com/niuxuewei/springplayground/controller/SimpleController.java
@@ -1,6 +1,8 @@
 package com.niuxuewei.springplayground.controller;
 
+import com.niuxuewei.springplayground.domain.CustomSettings;
 import com.niuxuewei.springplayground.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,9 @@ import java.util.Date;
 @RestController
 public class SimpleController {
 
+    @Autowired
+    private CustomSettings customSettings;
+
     @RequestMapping("/test")
     public String home() {
         return "Hello World!";
@@ -18,6 +23,11 @@ public class SimpleController {
     @GetMapping("/testjson")
     public User testjson() {
         return new User(11, "abc", "17615821400", new Date());
+    }
+
+    @GetMapping("/testproperties")
+    public Object testProperties() {
+        return customSettings;
     }
 
 }
---
+++ src/main/java/com/niuxuewei/springplayground/domain/CustomSettings.java
@@ -0,0 +1,31 @@
+package com.niuxuewei.springplayground.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource({"classpath:application.properties"})
+@ConfigurationProperties(prefix = "test")
+public class CustomSettings {
+
+    private String domain;
+
+    private String name;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
